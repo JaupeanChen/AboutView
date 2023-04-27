a6843fc7b79e5d41574a3e003402823bd5a7577e
Jarvan <1129561405@qq.com>
2023-04-27 17:35:20 +0800

    解决无法嵌套滑动问题

--- app/src/main/java/com/example/customdrugview/nestscroll/SuspendLayout.kt
+++ app/src/main/java/com/example/customdrugview/nestscroll/SuspendLayout.kt
@@ -15,23 +15,20 @@ import androidx.core.view.ViewCompat
  * Date: 2023/4/23
  * Description:
  */
-class SuspendLayout(
+class SuspendLayout @JvmOverloads constructor(
     context: Context?,
     attributeSet: AttributeSet? = null,
-) : LinearLayout(context, attributeSet), NestedScrollingParent3 {
+    def: Int = 0
+) : LinearLayout(context, attributeSet, def), NestedScrollingParent3 {
     private val tag = "NestScroll"
 
     private var headerChildHeight = 0
     private val scrollingParentHelper = NestedScrollingParentHelper(this)
 
     override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
-        Log.d(tag, "onStartNestedScroll")
+        Log.d(tag, "onStartNestedScroll: ${(axes and ViewCompat.SCROLL_AXIS_VERTICAL) != 0}")
         return (axes and ViewCompat.SCROLL_AXIS_VERTICAL) != 0
     }
-
-//    override fun onStartNestedScroll(child: View?, target: View?, nestedScrollAxes: Int): Boolean {
-//        return onStartNestedScroll(child!!, target!!, nestedScrollAxes, ViewCompat.TYPE_TOUCH)
-//    }
 
     override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
         Log.d(tag, "onNestedScrollAccepted")
@@ -47,6 +44,7 @@ class SuspendLayout(
     //为什么嵌套滑动机制要先做PreScroll的意义在这里就体现出来了，我们希望父类先滑动，接着子类再滑动
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
         Log.d(tag, "onNestedPreScroll")
+        Log.d(tag, "dy: $dy")
         //上划的时候我们（父类）先消费
         if (dy > 0) {
             //int deltaY = mLastMotionY - y;
@@ -92,22 +90,35 @@ class SuspendLayout(
         //TODO 这边下滑为什么要判断<0？
         //int deltaY = mLastMotionY - y;
         //final int unconsumedY = deltaY - scrolledDeltaY;
-        //但这里final int scrolledDeltaY = getScrollY() - oldY; scrolledDeltaY为正值，unconsumedY不是会变成更大的负值
+        //但这里final int scrolledDeltaY = getScrollY() - oldY;
+        //下拉的时候mScrollY会变小，那么再减去前一个值(oldY)的时候就是负值了。
+        //其实mScrollY就是顶部滚出屏幕(父容器)的值(像素)。
         //unconsumedY不是会变成更大的负值，有点说不通
         Log.d(tag, "dyUnconsumed: $dyUnconsumed")
-        //分发的时候用本次滑动的距离deltaY去减掉子view消费消费掉的距离scrolledDeltaY
+        //分发的时候用本次滑动的距离deltaY去减掉子view消费消费掉的距离scrolledDeltaY?
+        //其实这里dyUnconsumed算出来是负值, 传给scrollBy(0, dy), scrollBy内部再把
+        //原本的mScrollY减去dyUnconsumed, 相当于就把mScrollY变小了。
         if (dyUnconsumed < 0) scrollDown(dyUnconsumed, consumed)
     }
 
+    /**
+     * 为什么一开始可以无限上划，就是因为没有限制滑动上限，通过scrollBy(0, dy)传过来的上划距离越来越大。
+     * TODO 至于什么内嵌的NestedScrollView滑不下来？
+     * 因为一整个视图都被我们的父容器滑上去了，子view(NestedScrollView)并没有产生向上的mScrollY，
+     * 所以也就没有在内嵌的NestedScrollView内部下滑之说了。
+     */
     override fun scrollTo(x: Int, y: Int) {
         //限制自己的滑动范围，最多只能滑动header（第一个view）的高度，
         //这样一来也就达到了第二个view悬停的效果
-        MathUtils.clamp(y, 0, headerChildHeight)
-        super.scrollTo(x, y)
+        Log.d(tag, "scrollTo, headerChildHeight: $headerChildHeight")
+        Log.d(tag, "scrollTo, y: $y")
+        val valid = MathUtils.clamp(y, 0, headerChildHeight)
+        super.scrollTo(x, valid)
     }
 
     //测量自己第一个子view的高度，呜呜，自定义view三部曲的知识点又得重拾一下了T_T
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.d(tag, "onMeasure: $childCount")
         if (childCount > 0) {
             val headerView = getChildAt(0)
             measureChildWithMargins(headerView, widthMeasureSpec, 0, MeasureSpec.UNSPECIFIED, 0)
@@ -129,15 +140,17 @@ class SuspendLayout(
 
     private fun scrollUp(dy: Int, consumed: IntArray) {
         val oldScroll = scrollY
+        Log.d("TestScroll", "scrollUp: oldScroll = $scrollY")
         scrollBy(0, dy)
+        Log.d("TestScroll", "scrollUp: curScroll = $scrollY")
         consumed[1] = scrollY - oldScroll
     }
 
     private fun scrollDown(dyUnconsumed: Int, consumed: IntArray?) {
         val oldScroll = scrollY
-        if (dyUnconsumed > 0) {
-            scrollBy(0, dyUnconsumed)
-        }
+        Log.d("TestScroll", "scrollDown: oldScroll = $scrollY")
+        scrollBy(0, dyUnconsumed)
+        Log.d("TestScroll", "scrollDown: curScroll = $scrollY")
         val myConsume = scrollY - oldScroll
         if (consumed != null) {
             consumed[1] += myConsume
@@ -145,5 +158,4 @@ class SuspendLayout(
     }
 
     //--------------mine end ----------------------
-
 }
